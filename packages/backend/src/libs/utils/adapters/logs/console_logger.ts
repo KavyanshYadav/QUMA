5f7835053034cad75ba284076dd9c6abd6622a0e
KavyanshYadav <kavyansh.yadav.dev@gmail.com>
2025-10-04 23:51:02 +0530

    [feat] : added file and console log adapters

---
+++ packages/backend/src/libs/utils/adapters/logs/console_logger.ts
@@ -0,0 +1,24 @@
+import { logBase, LogLevel } from '../../../log/log.base';
+
+export class ConsoleAdapter implements logBase {
+  log(level: LogLevel, message: string, ...args: unknown[]) {
+    const output = `[${new Date().toISOString()}] [${level.toUpperCase()}] ${message}`;
+
+    switch (level) {
+      case 'error':
+        console.error(output, ...args);
+        break;
+      case 'warn':
+        console.warn(output, ...args);
+        break;
+      case 'debug':
+        console.debug(output, ...args);
+        break;
+      case 'info':
+        console.info(output, ...args);
+        break;
+      default:
+        console.log(output, ...args);
+    }
+  }
+}
